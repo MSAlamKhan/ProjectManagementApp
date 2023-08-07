8cd55dc75bdbcc83a6376d0d01f21c39c87641cd
MSAlamKhan <100859403+MSAlamKhan@users.noreply.github.com>
2023-08-07 10:03:55 +0500

    Delete ios/Pods directory

--- ios/Pods/Target Support Files/Pods-Lee_App-Lee_AppTests/Pods-Lee_App-Lee_AppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lee_App_Lee_AppTests : NSObject
-@end
-@implementation PodsDummy_Pods_Lee_App_Lee_AppTests
-@end
--- ios/Pods/Target Support Files/Pods-Lee_App/Pods-Lee_App-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lee_App : NSObject
-@end
-@implementation PodsDummy_Pods_Lee_App
-@end
